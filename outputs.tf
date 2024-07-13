25609261ad8f919cdf6f1084e6036767191c7c5d
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 19:10:48 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+/* BASEEEEEEEEEEE
 output "instance_id" {
   description = "The ID of the EC2 instance"
   value       = aws_instance.vm1.id
@@ -21,4 +22,31 @@ output "public_dns" {
 output "private_dns" {
   description = "The private DNS of the EC2 instance"
   value       = aws_instance.vm1.private_dns
+}
+*/
+
+
+output "instance_ids" {
+  description = "The IDs of the EC2 instances"
+  value       = { for k, v in aws_instance.vm : k => v.id }
+}
+
+output "public_ips" {
+  description = "The public IPs of the EC2 instances"
+  value       = { for k, v in aws_instance.vm : k => v.public_ip }
+}
+
+output "private_ips" {
+  description = "The private IPs of the EC2 instances"
+  value       = { for k, v in aws_instance.vm : k => v.private_ip }
+}
+
+output "public_dns" {
+  description = "The public DNS of the EC2 instances"
+  value       = { for k, v in aws_instance.vm : k => v.public_dns }
+}
+
+output "private_dns" {
+  description = "The private DNS of the EC2 instances"
+  value       = { for k, v in aws_instance.vm : k => v.private_dns }
 }
