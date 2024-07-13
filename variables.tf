504d50bd2730492a22ee4fdd3b43e94a202a02cb
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 18:32:28 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "instance_name" {
+  description = "The name of the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID to launch the instance in"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "A list of security group IDs to associate with the instance"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the instance"
+  type        = map(string)
+  default     = {}
+}
