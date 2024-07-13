75a54310bd94df3e0b2a73438b3bcb70f2105b58
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 19:03:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-resource "aws_instance" "vm1" {
+locals {
+  instances = {
+    web1 = { name = "web1", az = "eu-west-3a" },
+    web2 = { name = "web2", az = "eu-west-3b" }
+  }
+}
+
+resource "random_pet" "id" {
+  length    = 2
+  separator = "-"
+}
+
+resource "aws_instance" "vm" {
+  for_each               = local.instances
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
@@ -6,6 +19,6 @@ resource "aws_instance" "vm1" {
   vpc_security_group_ids = var.security_group_ids
 
   tags = merge({
-    Name = var.instance_name
+    Name = "${each.value.name}-${random_pet.id.id}"  # Dynamic name with random pet id
   }, var.tags)
 }
