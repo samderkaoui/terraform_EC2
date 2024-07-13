48b6c2e97b25433753b3e61bca8b43ee689338a2
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 19:16:00 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,49 @@
-/*
-ici c'est pour la BASE
+/* BASE
 variable "instance_name" {
   description = "The name of the EC2 instance"
   type        = string
 }
+
+variable "instance_type" {
+  description = "The type of the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID to launch the instance in"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "A list of security group IDs to associate with the instance"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the instance"
+  type        = map(string)
+  default     = {}
+}
 */
+
+variable "instances" {
+  description = "A map of instance configurations"
+  type        = map(object({
+    name = string
+    az   = string
+  }))
+}
 
 variable "ami" {
   description = "The AMI to use for the instance"
@@ -22,9 +61,9 @@ variable "key_name" {
   type        = string
 }
 
-variable "subnet_id" {
-  description = "The subnet ID to launch the instance in"
-  type        = string
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
 }
 
 variable "security_group_ids" {
