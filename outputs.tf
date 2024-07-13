6236dc7f46c29dd1ffc22248c93f169e2e1b74e9
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 18:46:05 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.ec2.id
+  value       = aws_instance.vm1.id
 }
 
 output "public_ip" {
   description = "The public IP of the EC2 instance"
-  value       = aws_instance.ec2.public_ip
+  value       = aws_instance.vm1.public_ip
 }
 
 output "private_ip" {
   description = "The private IP of the EC2 instance"
-  value       = aws_instance.ec2.private_ip
+  value       = aws_instance.vm1.private_ip
 }
 
 output "public_dns" {
   description = "The public DNS of the EC2 instance"
-  value       = aws_instance.ec2.public_dns
+  value       = aws_instance.vm1.public_dns
 }
 
 output "private_dns" {
   description = "The private DNS of the EC2 instance"
-  value       = aws_instance.ec2.private_dns
+  value       = aws_instance.vm1.private_dns
 }
