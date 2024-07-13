0def09399015a1a8c54f0a87eddcd23cfadc224e
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 19:38:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ resource "aws_instance" "vm" {
   vpc_security_group_ids = var.security_group_ids
 
   tags = merge({
-    Name = "${each.value.name}"  # Dynamic name with random pet id
+    Name = "${each.value.name}"
   }, var.tags)
 }
