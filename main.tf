b122349f18da12d1fd35ac1dccc03542a6b3998f
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-30 10:21:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "vm" {
   subnet_id              = each.key == "web1" ? var.public_subnets[0] : var.public_subnets[1]
   vpc_security_group_ids = var.security_group_ids
 
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
+  }
+
+
   tags = merge({
     Name = "${each.value.name}"
   }, var.tags)
